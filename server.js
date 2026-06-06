501949f926fc5e65a44029c2a409bf4687e78c48
pineapple2508 <rokhimahlatifatur@gmail.com>
2026-06-07 00:32:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage: storage });
 
+app.set("views", path.join(__dirname, "views"));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
