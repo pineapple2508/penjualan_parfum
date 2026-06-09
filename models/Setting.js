8b38351bffcadbb36bdf3e966532542fa1677814
pineapple2508 <rokhimahlatifatur@gmail.com>
2026-06-09 14:46:46 +0700

    Update Setting.js

--- models/Setting.js
+++ models/Setting.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const schema = new mongoose.Schema({
   homeTitle: { type: String, default: 'FATURÉ' },
   homeSubtitle: { type: String, default: 'Aroma elegan untuk setiap kesan yang tak terlupakan' },
+   bannerImage: { type: String, default: ''},
   homeAboutText: { type: String, default: 'Di Faturé, kami percaya bahwa parfum bukan sekadar aroma, melainkan identitas. Kami meracik setiap tetesan esensi dengan bahan premium pilihan untuk memastikan kesan mewah dan tahan lama. Temukan aroma yang mewakili diri Anda.' },
   aboutTitle: { type: String, default: 'Tentang Faturé' },
   aboutTagline: { type: String, default: 'Mendefinisikan Elegansi dalam Setiap Tetesan' },
