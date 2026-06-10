3a0a84f73680ee8326b7f233d44515cfe1ea4230
pineapple2508 <rokhimahlatifatur@gmail.com>
2026-06-10 16:25:32 +0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  username: { type: String, unique: true, required: true },
-  email: { type: String, unique: true, required: true },
+  username: { type: String, required: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
   role: { type: String, default: 'customer' },
   isVerified: { type: Boolean, default: false }, // TAMBAHAN: Status verifikasi email
